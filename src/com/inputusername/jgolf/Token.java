e56595222e0679cbc971c307f7b50a6addbbf437
Koen Bolhuis <Koen Bolhuis>
2016-07-16 18:52:25 +0200

    Started rewrite of tokenizer

--- src/com/inputusername/jgolf/Token.java
+++ src/com/inputusername/jgolf/Token.java
@@ -16,34 +16,9 @@ class Token {
     private String tokenString;
     private Type tokenType;
 
-    private Token(String tokenString, Type tokenType) {
+    Token(String tokenString, Type tokenType) {
         this.tokenString = tokenString;
         this.tokenType = tokenType;
-    }
-
-    Token(String tokenString) {
-        this(tokenString, findTokenType(tokenString));
-    }
-
-    private static Type findTokenType(String tokenString) {
-        if (tokenString.matches("[a-zA-Z_][a-zA-Z0-9_]*")) {
-            return Type.WORD;
-        }
-        else if (tokenString.matches("-?[0-9]+")) {
-            return Type.NUMBER;
-        }
-        else if (tokenString.startsWith("{") && tokenString.endsWith("}")) {
-            return Type.BLOCK;
-        }
-        else if (tokenString.matches("'(?:\\.|[^'])*'?") || tokenString.matches("\"(?:\\.|[^\"])*\"?")) {
-            return Type.STRING;
-        }
-        else if (tokenString.startsWith("#")) {
-            return Type.COMMENT;
-        }
-        else {
-            return Type.OTHER;
-        }
     }
 
     public String getTokenString() {
--- src/com/inputusername/jgolf/Tokenizer.java
+++ src/com/inputusername/jgolf/Tokenizer.java
@@ -2,8 +2,7 @@ package com.inputusername.jgolf;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.logging.Logger;
 
 /**
  * Created by Koen on 3-7-2016.
@@ -13,18 +12,154 @@ import java.util.regex.Pattern;
 //TODO: support escaped strings
 
 class Tokenizer {
-    // [a-zA-Z_][a-zA-Z_]*|-?[0-9]+|\\{[^\\}]*\\}|'(?:\\.|[^'])*'?|\"(?:\\.|[^\"])*\"?|#[^\n\r]*|.
-    private final static String REGEX = "[a-zA-Z_][a-zA-Z_]*|-?[0-9]+|\\{[^\\}]*\\}|'(?:\\.|[^'])*'?|\"(?:\\.|[^\"])*\"?|#[^\n\r]*|.";
-    private final static Pattern pattern = Pattern.compile(REGEX);
-
     static List<Token> tokenize(String code) {
         List<Token> tokens = new ArrayList<>();
 
-        Matcher matcher = pattern.matcher(code);
+        Log.p("starting tokenization");
 
-        while (matcher.find()) {
-            Token token = new Token(matcher.group());
-            tokens.add(token);
+        int length = code.length();
+        for (int i = 0; i < length; i++) {
+            char c = code.charAt(i);
+
+            Log.p("char #" + i + ": " + c);
+
+            if (c == '#') {
+                // Comments
+
+                int start = i;
+
+                do {
+                    i++;
+                    if (i == code.length()) {
+                        break;
+                    }
+                } while (code.charAt(i) != '\n');
+
+                Token token = new Token(code.substring(start, i), Token.Type.COMMENT);
+                tokens.add(token);
+
+                i--;
+            }
+            else if (c == '"' || c == '\'') {
+                // Strings
+
+                char startChar = c;
+
+                StringBuilder builder = new StringBuilder();
+
+                builder.append(startChar);
+
+                do {
+                    i++;
+                    if (i == code.length()) {
+                        break;
+                    }
+                    c = code.charAt(i);
+
+                    if (c == '\\') {
+                        char escape = code.charAt(i + 1);
+                        switch (escape) {
+                            case '0': builder.append('\0'); break;
+                            case 'b': builder.append('\b'); break;
+                            case 't': builder.append('\t'); break;
+                            case 'n': builder.append('\n'); break;
+                            case 'f': builder.append('\f'); break;
+                            case 'r': builder.append('\r'); break;
+                            case '\\': builder.append('\\'); break;
+                            default: builder.append(escape); break;
+                        }
+                        i++;
+                    }
+                    else {
+                        builder.append(c);
+                    }
+                } while (c != startChar);
+
+                Token token = new Token(builder.toString(), Token.Type.STRING);
+                tokens.add(token);
+
+                i--;
+
+                Log.p("string token: " + token.getTokenString());
+            }
+            else if (c == '{') {
+                // Blocks
+
+                StringBuilder builder = new StringBuilder();
+
+                builder.append('{');
+
+                int blockNesting = 1;
+
+                do {
+                    i++;
+                    if (i == code.length()) {
+                        break;
+                    }
+                    c = code.charAt(i);
+
+                    if (c == '{') {
+                        blockNesting++;
+                    }
+                    else if (c == '}') {
+                        blockNesting--;
+                    }
+
+                    builder.append(c);
+                } while (blockNesting != 0);
+
+                Token token = new Token(builder.toString(), Token.Type.BLOCK);
+                tokens.add(token);
+
+                i--;
+
+                Log.p("block token: " + token.getTokenString());
+            }
+            else if (Character.isDigit(c)) {
+                // Numbers
+
+                int start = i;
+
+                do {
+                    i++;
+                    if (i == code.length()) {
+                        break;
+                    }
+                } while (Character.isDigit(code.charAt(i)));
+
+                Token token = new Token(code.substring(start, i), Token.Type.NUMBER);
+                tokens.add(token);
+
+                i--;
+
+                Log.p("number token: " + token.getTokenString());
+            }
+            else if (c == '_' || Character.isAlphabetic(c)) {
+                // Words
+
+                int start = i;
+
+                do {
+                    i++;
+                    if (i == code.length()) {
+                        break;
+                    }
+                    c = code.charAt(i);
+                } while (c == '_' || Character.isLetterOrDigit(c));
+
+                Token token = new Token(code.substring(start, i), Token.Type.WORD);
+                tokens.add(token);
+
+                Log.p("word token: " + token.getTokenString());
+            }
+            else {
+                // Other
+
+                Log.p("other token: " + c);
+
+                Token token = new Token(Character.toString(c), Token.Type.OTHER);
+                tokens.add(token);
+            }
         }
 
         return tokens;
