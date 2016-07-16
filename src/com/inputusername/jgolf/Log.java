e0bc88348b7eaad868eaf6012a4944ca130665ab
Koen Bolhuis <Koen Bolhuis>
2016-07-16 18:52:10 +0200

    Added logger class

---
+++ src/com/inputusername/jgolf/Log.java
@@ -0,0 +1,11 @@
+package com.inputusername.jgolf;
+
+/**
+ * Created by Koen on 16-7-2016.
+ */
+class Log {
+    static void p(String msg) {
+        System.out.println(
+                msg);
+    }
+}
